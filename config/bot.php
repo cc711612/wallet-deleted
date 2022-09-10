76129dc7a7d66ce2da3cbb5178b74afb2d35a52c
Roy <cc711612@gmail.com>
2022-09-10 16:22:09 +0800

    新增line名單

--- config/bot.php
+++ config/bot.php
@@ -4,6 +4,6 @@ return [
     'line' => [
         'access_token'   => env('LINE_BOT_TOKEN'),
         'channel_secret' => env('LINE_BOT_SECRET'),
-        'admin_user_ids'  => env('LINE_ADMIN_USERID', ['U1d40789aa8461e74ead62181b1abc442']),
+        'admin_user_ids'  => env('LINE_ADMIN_USERID', ['U1d40789aa8461e74ead62181b1abc442','Uc4102f98704f54c9cecd8448f4a462e5']),
     ],
 ];
